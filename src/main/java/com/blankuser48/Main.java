89fe68b1ea90b7e148a1135c645d6a80400c8a91
MosqitoTorpedo <mosqitotorpedo@gmail.com>
2024-01-04 19:43:09 -0600

    oh god

--- src/main/java/com/blankuser48/Main.java
+++ src/main/java/com/blankuser48/Main.java
@@ -1,21 +1,44 @@
 package com.blankuser48;
 
+import jdk.internal.util.xml.impl.Input;
+
 import java.awt.*;
 import java.awt.event.InputEvent;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws AWTException, InterruptedException {
-
-        Thread.sleep(1000);
-
         Robot robot = new Robot();
-        robot.setAutoDelay(1000);
+        int MOUSE_BUTTON_ONE =InputEvent.getMaskForButton(1);
 
-        int mouseButtonOne = InputEvent.getMaskForButton(1);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How long should each click last in milliseconds?");
+        int delay = Integer.parseInt(scanner.nextLine());
+        System.out.println(String.format("Each click will last %s milliseconds", delay));
+        System.out.println("How long should it click in seconds?");
+        int length = Integer.parseInt(scanner.nextLine());
+        robot.setAutoDelay(delay);
 
-        robot.mousePress(mouseButtonOne);
-        robot.mouseRelease(mouseButtonOne);
+
+        System.out.println("Okay clicking in 5 seconds!");
+        Thread.sleep(5000);
+
+        int time = length * delay;
+
+        boolean running = true;
+        while (running) {
+            robot.mousePress(MOUSE_BUTTON_ONE);
+            robot.mouseRelease(MOUSE_BUTTON_ONE);
+            if (time != 0) {
+                time--;
+            } else {
+                running = false;
+            }
+        }
 
 
     }
+
+
 }
+
