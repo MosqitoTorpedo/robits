b0044850c87027f7c281fb1f130212a65e722842
MosqitoTorpedo <mosqitotorpedo@gmail.com>
2024-01-04 18:57:46 -0600

    checkpoint

--- src/main/java/com/blankuser48/Main.java
+++ src/main/java/com/blankuser48/Main.java
@@ -1,7 +1,21 @@
 package com.blankuser48;
 
+import java.awt.*;
+import java.awt.event.InputEvent;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws AWTException, InterruptedException {
+
+        Thread.sleep(1000);
+
+        Robot robot = new Robot();
+        robot.setAutoDelay(1000);
+
+        int mouseButtonOne = InputEvent.getMaskForButton(1);
+
+        robot.mousePress(mouseButtonOne);
+        robot.mouseRelease(mouseButtonOne);
+
+
     }
 }
